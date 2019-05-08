94aad40df8d6cb7ea93428b510b7337888c219f8
qcmiao1998 <qcmiao@hotmail.com>
2019-05-09 00:05:52 +0800

    添加项目文件。

---
+++ YY_Navigation/YY_Navigation/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace YY_Navigation
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Title = "YY Navigation";
+            WebsiteInfos.Init();
+            InitializeComponent();
+        }
+
+        private void WebTapped(object sender, ItemTappedEventArgs e)
+        {
+            var myListView = (ListView)sender;
+            var myItem = (WebsiteInfos.Website)myListView.SelectedItem;
+            myListView.SelectedItem = null;
+            Browser.OpenAsync(myItem.Uri, BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
---
+++ YY_Navigation/YY_Navigation/WebsiteInfos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YY_Navigation
+{
+    public class WebsiteInfos
+    {
+        public static List<Website> Websites { get; private set; }
+        public static void Init()
+        {
+            Websites = new List<Website>
+            {
+                new Website(@"https://www.moonhe.com/", "Moonhe", "d3d3Lm1vb25oZS5jb20.png"),
+                new Website(@"https://nobugs.dev/", "Fuma's gadget", "bm9idWdzLmRldg.jpg"),
+                new Website(@"https://iamsmally.github.io/", "SmallY's Home", "yy.jpg"),
+                new Website(@"https://mizunashi.me/", "Mizunashi's Blog", "mizunashi.png"),
+                new Website(@"https://www.gofun4.top/", "番茄瓜皮自嗨的地方", "fun4wut.gif")
+            };
+        }
+
+
+        public class Website
+        {
+            public Website(string uri, string name, string imageUri)
+            {
+                Uri = uri;
+                Name = name;
+                ImageUri = imageUri;
+            }
+            public Website(){}
+            public string Uri { get; set; }
+            public string Name { get; set; }
+            public string ImageUri { get; set; }
+        }
+
+    }
+}
